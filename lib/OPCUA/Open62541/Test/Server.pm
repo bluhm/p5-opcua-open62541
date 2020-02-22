af0b3537f56fa49c990e7e57652f8b17aebdf964
Alexander Bluhm <alexander.bluhm@gmx.net>
2020-02-22 13:37:53 +0100

    Implement OPCUA::Open62541::Test::Server testing package.
    The client tests often start a server.  This can be implemented as
    a generic class.  The functionality may also be useful in the
    installed version of this module.

---
+++ lib/OPCUA/Open62541/Test/Server.pm
@@ -0,0 +1,160 @@
+package OPCUA::Open62541::Test::Server;
+
+use strict;
+use warnings;
+use Net::EmptyPort qw(empty_port);
+use OPCUA::Open62541 ':statuscode';
+use POSIX qw(sigaction SIGTERM SIGALRM);
+
+use Test::More;
+
+sub planning {
+    # number of ok() and is() calls in this code
+    return 8;
+}
+
+sub new {
+    my $class = shift;
+    my %args = @_;
+    my $self = {};
+    $self->{timeout} = $args{timeout} || 10;
+
+    ok($self->{server} = OPCUA::Open62541::Server->new(), "server");
+    ok($self->{config} = $self->{server}->getConfig(), "get server config");
+
+    return bless($self, $class);
+}
+
+sub start {
+    my $self = shift;
+
+    ok($self->{port} = empty_port(), "empty port");
+    diag("going to configure server");
+    is($self->{config}->setMinimal($self->{port}, ""), STATUSCODE_GOOD,
+	"set minimal server config port $self->{port}");
+
+    $self->{pid} = fork();
+    if (defined($self->{pid})) {
+	if ($self->{pid} == 0) {
+	    $self->child();
+	    POSIX::_exit(0);
+	}
+	ok(1, "fork server");
+    } else {
+	ok(0, "fork server") or diag "Fork failed: $!";
+    }
+
+    # XXX should way until server did bind(2) the port,
+    # may grep for 'TCP network layer listening on' in server log
+}
+
+sub child {
+    my $self = shift;
+
+    my $running = 1;
+    my $handler = sub {
+	$running = 0;
+    };
+
+    # Perl signal handler only works between perl statements.
+    # Use the real signal handler to interrupt the OPC UA server.
+    # This is not signal safe, best effort is good enough for a test.
+    my $sigact = POSIX::SigAction->new($handler)
+	or die "could not create POSIX::SigAction";
+    sigaction(SIGTERM, $sigact)
+	or die "sigaction SIGTERM failed: $!";
+    sigaction(SIGALRM, $sigact)
+	or die "sigaction SIGALRM failed: $!";
+    defined(alarm($self->{timeout}))
+	or die "alarm failed: $!";
+
+    # run server and stop after ten seconds or due to kill
+    diag("going to startup server");
+    $self->{server}->run($running);
+}
+
+sub stop {
+    my $self = shift;
+
+    diag("going to shutdown server");
+    ok(kill(SIGTERM, $self->{pid}), "kill server");
+    is(waitpid($self->{pid}, 0), $self->{pid}, "waitpid");
+    is($?, 0, "server finished");
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+OPCUA::Open62541::Test::Server - run open62541 server for testing
+
+=head1 SYNOPSIS
+
+  use OPCUA::Open62541::Test::Server;
+  use Test::More tests => OPCUA::Open62541::Test::Server::planning();
+
+  my $server = OPCUA::Open62541::Test::Server->new();
+
+=head1 DESCRIPTION
+
+In a module test start and run an open62541 OPC UA server in the
+background that can be connected by a client.
+The server is considered part of the test and will write to the TAP
+stream.
+
+=over 4
+
+=item OPCUA::Open62541::Test::Server::planning
+
+Return the number of tests results that running one server will
+create.
+Add this to your number of planed tests.
+
+=back
+
+=head2 METHODS
+
+=over 4
+
+=item $server = OPCUA::Open62541::Test::Server->new(%args);
+
+Create a new test server instance.
+
+=over 8
+
+=item $args{timeout}
+
+Maximum time the server will run before shutting down itself.
+Defaults to 10 seconds.
+
+=back
+
+=item $server->start()
+
+Startup the open62541 server as a background process.
+The function will return immediately.
+
+=item $server->stop()
+
+Stop the background server and check its exit code.
+
+=back
+
+=head1 SEE ALSO
+
+OPCUA::Open62541
+
+=head1 AUTHORS
+
+Alexander Bluhm E<lt>bluhm@genua.deE<gt>,
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (c) 2020 Alexander Bluhm E<lt>bluhm@genua.deE<gt>
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
