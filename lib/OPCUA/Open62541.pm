d132b7e0d90060ec12fbbaf3ec033a5290540fb0
Alexander Bluhm <alexander.bluhm@gmx.net>
2022-05-30 20:11:52 +0200

    Bump version number to 1.00.

--- lib/OPCUA/Open62541.pm
+++ lib/OPCUA/Open62541.pm
@@ -7,7 +7,7 @@ require Exporter;
 use parent 'Exporter';
 use OPCUA::Open62541::Constant;
 
-our $VERSION = '0.030';
+our $VERSION = '1.00';
 
 our @EXPORT_OK = @OPCUA::Open62541::Constant::EXPORT_OK;
 our %EXPORT_TAGS = %OPCUA::Open62541::Constant::EXPORT_TAGS;
