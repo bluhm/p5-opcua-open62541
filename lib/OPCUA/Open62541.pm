8255bf9844991bdd2145bfb5f879a2706d258dbb
Alexander Bluhm <alexander.bluhm@gmx.net>
2023-01-25 17:15:36 +0100

    Bump version number to 2.00 and update change log.
    The removal of the open62541 1.0 and 1.1 API compatibility justifies
    a new major version.

--- lib/OPCUA/Open62541.pm
+++ lib/OPCUA/Open62541.pm
@@ -7,7 +7,7 @@ require Exporter;
 use parent 'Exporter';
 use OPCUA::Open62541::Constant;
 
-our $VERSION = '1.06';
+our $VERSION = '2.00';
 
 our @EXPORT_OK = @OPCUA::Open62541::Constant::EXPORT_OK;
 our %EXPORT_TAGS = %OPCUA::Open62541::Constant::EXPORT_TAGS;
