be350fbcf83ca0d3697af3894ebc6575af6b1e8d
Anton Borowka <aboro@genua.de>
2025-06-24 11:48:21 +0200

    Implement some higher level client functionality
    This adds some functions to easily read and browse with a
    OPCUA::Open62541::Client.
    
    * Creates request structures from easy inputs
    * Returns relevant results
    * Browse continuation points are automatically followed vie BrowseNext
    * ServiceLevel errors cause a DIE
    
    Still missing some documentation and tests.

--- lib/OPCUA/Open62541.pm
+++ lib/OPCUA/Open62541.pm
@@ -72,6 +72,97 @@ XSLoader::load('OPCUA::Open62541', $VERSION);
 
 1;
 
+package OPCUA::Open62541::Client;
+
+use strict;
+use warnings;
+
+my %attributeid_names = OPCUA::Open62541::get_mapping_attributeid_names;
+
+sub get_attributes {
+    my ($self, $nodeid, @attributes) = @_;
+
+    # is there any default that
+    die 'no attributes for get_attributes'
+	if not @attributes;
+
+    @attributes = map { $attributeid_names{$_} // $_ } @attributes;
+
+    my $response = $self->Service_read({
+	ReadRequest_nodesToRead => [
+	    map {
+		{ReadValueId_nodeId => $nodeid, ReadValueId_attributeId => $_}
+	    } @attributes
+	],
+    });
+
+    my $status = $response->{"ReadResponse_responseHeader"}{ResponseHeader_serviceResult};
+    die "Read failed with $status"
+	if $status ne 'Good';
+
+    return @{$response->{ReadResponse_results} // []};
+}
+
+sub get_references {
+    my ($self, $nodeid, %args) = @_;
+
+    my $result_mask       = $args{result_mask};
+    my $include_subtypes  = $args{include_subtypes} // 1;
+    my $browse_direction  = $args{browse_direction} // 0;
+    my $reference_type_id = $args{reference_type_id} // {
+	NodeId_namespaceIndex => 0,
+	NodeId_identifierType => OPCUA::Open62541::NODEIDTYPE_NUMERIC,
+	NodeId_identifier => OPCUA::Open62541::NS0ID_REFERENCES,
+    };
+
+    my $request = {
+	BrowseRequest_requestedMaxReferencesPerNode => 0,
+	BrowseRequest_nodesToBrowse => [{
+	    BrowseDescription_nodeId => $nodeid,
+	    BrowseDescription_browseDirection => $browse_direction,
+	    BrowseDescription_referenceTypeId => $reference_type_id,
+	    BrowseDescription_includeSubtypes => $include_subtypes,
+	    BrowseDescription_resultMask => $result_mask,
+	}],
+    };
+
+    my $type = 'Browse';
+    my @references;
+  NEXT:
+    my $response = $type eq 'Browse' ? $self->Service_browse($request)
+	: $self->Service_browseNext($request);
+
+    my $status = $response->{"${type}Response_responseHeader"}{ResponseHeader_serviceResult};
+    die "$type failed with $status"
+	if $status ne 'Good';
+
+    my $result = $response->{"${type}Response_results"}[0];
+    push @references,
+	@{$result->{BrowseResult_references} // []};
+
+    if (my $continuation_point = $result->{"BrowseResult_continuationPoint"}) {
+	$type = 'BrowseNext';
+	$request = {
+	    BrowseNextRequest_continuationPoints => [$continuation_point]
+	};
+	goto NEXT;
+    }
+
+    return @references
+}
+
+sub get_references_hierarchical {
+    get_references(
+	@_, reference_type_id => {
+	    NodeId_namespaceIndex => 0,
+	    NodeId_identifier     => OPCUA::Open62541::NS0ID_HIERARCHICALREFERENCES,
+	    NodeId_identifierType => OPCUA::Open62541::NODEIDTYPE_NUMERIC,
+	},
+    );
+}
+
+1;
+
 __END__
 
 =pod
